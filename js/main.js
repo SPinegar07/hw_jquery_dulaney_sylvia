56d10b82397317739dfa4d80d48594eeaedd47f7
Sylvia Dulaney <SPinegar07@ufl.edu>
2015-11-20 16:03:18 -0600

    styled paragraph text

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@ $(document).ready(function(){
 $( document ).ready(function() {
     var paragraphs = $( 'p' )
 });
+
+$( 'p' ).css({
+	"background-color":"#c4e1ac", 
+	"color": "#4e69a2", 
+	"lineHeight": 1.25, 
+	"fontWeight": "semi-bold",
+	"padding-left": "15px",
+	"font-size":".75em",
+	"font-family":"Righteous",
+});
