4831bc01a373bb04a9717ac4db9c049a6562fb33
Sylvia Dulaney <SPinegar07@ufl.edu>
2015-11-20 14:27:56 -0600

    fiddled with creating jquery replacement script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
     // jQuery/JavaScript code goes here
 });
+
+$( document ).ready(function() {
+    var headings = $( 'h2' )
+});
+
+
+$(document).ready(function(){
+    $( 'h2' ).last().html( '<h2>I'm Sylvia Pinegar, and I'm learning jQuery</h2>' ); 
+});
